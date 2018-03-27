938cef6cd5c34bce13a7cfabb05eae172ec401d9
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-03-27 21:08:40 +0900

    [add]i3blocks

--- install/ln.sh
+++ install/ln.sh
@@ -1,4 +1,5 @@
 ln -s ~/settings/vim/.vimrc ~/.vimrc
+ln -s ~/settings/i3/i3blocks.conf ~/.config/i3blocks/config 
 ln -s ~/settings/i3/config ~/.config/i3/config
 ln -s ~/settings/vim/dein.toml ~/.vim/dein/dein.toml
 

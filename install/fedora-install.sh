bbd26806449151a2f91119de887fe481654715cd
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-04-28 10:11:03 +0900

    [fix]fedora install

--- install/fedora-install.sh
+++ install/fedora-install.sh
@@ -1,23 +1,21 @@
-yum update -y
-yum install -y fontawesome-fonts
-yum install -y powerline-fonts
-yum install -y i3
-yum install -y vim
-yum install -y feh
-yum install -y ibus-mozc
-yum install -y dolphin
-yum install -y fish
-git clone https://github.com/TYuto/settings
+sudo yum update -y
+sudo yum install -y fontawesome-fonts
+sudo yum install -y powerline-fonts
+sudo yum install -y i3
+sudo yum install -y vim
+sudo yum install -y feh
+sudo yum install -y ibus-mozc
+sudo yum install -y dolphin
+sudo yum install -y fish
 mkdir /home/yuto/.vim/dein
-mkdir /home/yuto.config/i3
-mkdir /homr/yuto/config/i3blocks
+mkdir /home/yuto/.config/i3
 curl -L https://get.oh-my.fish | fish
 echo "yuto ALL=(ALL) ALL" >> /etc/sudoers
-yum install terminator
-yum install util-linux-user
-yum install -y screenfetch
-yum install -y gcc
+sudo yum install terminator
+sudo yum install util-linux-user
+sudo yum install -y screenfetch
+sudo yum install -y gcc
 sudo dnf copr enable mrbloups/compton
-dnf install compton
-yum install htop
+sudo dnf install compton
+sudo yum install htop
 
